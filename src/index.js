885b5623fc9e30386fc4135a1e6c2f47346f9bf4
tim <tim@glo.id.au>
2024-05-23 15:30:00 +1000

    more fixes to comment text

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ const main = async () => {
     const invalidFiles = [...arrayOutput];
 
     const successBody = `rocket: **Awesome**, your all assets are less than ${inputs.thrashold_size}Kb.`;
-    const errorBody = `:warning: **Oh, Snap**, You have ${count} asset(s) with size more than ${inputs.thrashold_size}Kb. Please optimize them. If you unable to optimize these assets, you can use .assets-ignore file and add these assets in .assets-ignore file`;
+    const errorBody = `:warning: **Oh, Snap**, You have ${count} asset(s) with size more than \`${inputs.thrashold_size}Kb\`. If you unable to optimize these assets, you can use \`.assets-ignore\` file and add these assets in \`.assets-ignore\` file`;
 
     const getTableDataString = (invalidFiles) => {
       const filteredFiles = [];
