4c29ea0632f72cacfe53afce355efc3158f31cd1
tim <tim@glo.id.au>
2024-05-23 14:36:42 +1000

    adding some debug output to try to work out why shit isnt working

--- src/index.js
+++ src/index.js
@@ -177,7 +177,11 @@ const main = async () => {
           comment.user.login === "github-actions[bot]" &&
           comment.body.includes(GITHUB_COMMENT_BOT_PREFIX)
         ) {
-          console.log("@@@ DELETING A COMMENT !!!", comment);
+          console.log("@@@ DELETING A COMMENT !!!", {
+            owner,
+            repo,
+            comment_id: comment.id,
+          });
 
           try {
             await octokit.rest.issues.deleteComment({
